d7674a67359fd503494463f0a7f4fe0743d604fc
Simon <simon.vonhoff@outlook.com>
2023-11-05 19:35:47 +0100

    Added generated documentation
    The inconsistent function formatting is intentional.
    The different formatting used are: Google, Microsoft, GNU

--- src/point.c
+++ src/point.c
@@ -2,52 +2,92 @@
 #include "library.h"
 #include <stdlib.h>
 
-point_t *
-point_create (uint32_t x, uint32_t y)
+/**
+ * @brief Create a new point with the specified coordinates.
+ *
+ * This function allocates memory for a new point structure and initializes its
+ * coordinates based on the provided values. The caller is responsible for freeing
+ * the memory allocated by this function when it is no longer needed.
+ *
+ * @param x The x-coordinate of the point.
+ * @param y The y-coordinate of the point.
+ *
+ * @return A pointer to the newly created point if allocation succeeds, or NULL if
+ * memory allocation fails.
+ */
+point_t *point_create(uint32_t x, uint32_t y)
 {
-  point_t *point = malloc (sizeof (struct point));
-  if (point != NULL)
+    point_t *point = malloc(sizeof(struct point));
+    if (point != NULL)
     {
-      point->x = x;
-      point->y = y;
+        point->x = x;
+        point->y = y;
     }
 
-  return point;
+    return point;
 }
 
-bool
-point_destroy (point_t *point)
+/**
+ * @brief Destroy a point and release its associated memory.
+ *
+ * This function frees the memory allocated for the given point. It should be called
+ * when the point is no longer needed to prevent memory leaks.
+ *
+ * @param point A pointer to the point to be destroyed.
+ *
+ * @return true if the point was successfully destroyed, false if the input point
+ * pointer is NULL.
+ */
+bool point_destroy(point_t *point)
 {
-  bool result = false;
-  if (point != NULL)
+    bool result = false;
+    if (point != NULL)
     {
-      free (point);
-      result = true;
+        free(point);
+        result = true;
     }
 
-  return result;
+    return result;
 }
 
-uint32_t
-point_get_x (const point_t *point)
+/**
+ * @brief Get the x-coordinate of a point.
+ *
+ * This function returns the x-coordinate of the given point. If the input point
+ * pointer is NULL, the function returns 0.
+ *
+ * @param point A pointer to the point to retrieve the x-coordinate from.
+ *
+ * @return The x-coordinate of the point, or 0 if the input point pointer is NULL.
+ */
+uint32_t point_get_x(const point_t *point)
 {
-  uint32_t result = 0U;
-  if (point != NULL)
+    uint32_t result = 0U;
+    if (point != NULL)
     {
-      result = point->x;
+        result = point->x;
     }
 
-  return result;
+    return result;
 }
 
-uint32_t
-point_get_y (const point_t *point)
+/**
+ * @brief Get the y-coordinate of a point.
+ *
+ * This function returns the y-coordinate of the given point. If the input point
+ * pointer is NULL, the function returns 0.
+ *
+ * @param point A pointer to the point to retrieve the y-coordinate from.
+ *
+ * @return The y-coordinate of the point, or 0 if the input point pointer is NULL.
+ */
+uint32_t point_get_y(const point_t *point)
 {
-  uint32_t result = 0U;
-  if (point != NULL)
+    uint32_t result = 0U;
+    if (point != NULL)
     {
-      result = point->y;
+        result = point->y;
     }
 
-  return result;
+    return result;
 }
--- src/rectangle.c
+++ src/rectangle.c
@@ -2,8 +2,24 @@
 #include "library.h"
 #include <stdlib.h>
 
+/**
+ * @brief Create a new rectangle with the specified parameters.
+ *
+ * This function allocates memory for a new rectangle structure and initializes its
+ * coordinates and dimensions based on the provided values. The caller is responsible
+ * for freeing the memory allocated by this function when it is no longer needed.
+ *
+ * @param x      The x-coordinate of the rectangle.
+ * @param y      The y-coordinate of the rectangle.
+ * @param width  The width of the rectangle.
+ * @param height The height of the rectangle.
+ *
+ * @return A pointer to the newly created rectangle if allocation succeeds, or NULL
+ *         if memory allocation fails.
+ */
 rectangle_t *
-rectangle_create (uint32_t x, uint32_t y, uint32_t width, uint32_t height)
+rectangle_create (uint32_t x, uint32_t y,
+                  uint32_t width, uint32_t height)
 {
   rectangle_t *rectangle = malloc (sizeof (struct rectangle));
   if (rectangle != NULL)
@@ -17,6 +33,17 @@ rectangle_create (uint32_t x, uint32_t y, uint32_t width, uint32_t height)
   return rectangle;
 }
 
+/**
+ * @brief Destroy a rectangle and release its associated memory.
+ *
+ * This function frees the memory allocated for the given rectangle. It should be
+ * called when the rectangle is no longer needed to prevent memory leaks.
+ *
+ * @param rectangle A pointer to the rectangle to be destroyed.
+ *
+ * @return true if the rectangle was successfully destroyed, false if the input
+ *         rectangle pointer is NULL.
+ */
 bool
 rectangle_destroy (rectangle_t *rectangle)
 {
@@ -30,6 +57,16 @@ rectangle_destroy (rectangle_t *rectangle)
   return result;
 }
 
+/**
+ * @brief Get the x-coordinate of a rectangle.
+ *
+ * This function returns the x-coordinate of the given rectangle. If the input
+ * rectangle pointer is NULL, the function returns 0.
+ *
+ * @param rectangle A pointer to the rectangle to retrieve the x-coordinate from.
+ *
+ * @return The x-coordinate of the rectangle, or 0 if the input rectangle pointer is NULL.
+ */
 uint32_t
 rectangle_get_x (const rectangle_t *rectangle)
 {
@@ -42,6 +79,16 @@ rectangle_get_x (const rectangle_t *rectangle)
   return result;
 }
 
+/**
+ * @brief Get the y-coordinate of a rectangle.
+ *
+ * This function returns the y-coordinate of the given rectangle. If the input
+ * rectangle pointer is NULL, the function returns 0.
+ *
+ * @param rectangle A pointer to the rectangle to retrieve the y-coordinate from.
+ *
+ * @return The y-coordinate of the rectangle, or 0 if the input rectangle pointer is NULL.
+ */
 uint32_t
 rectangle_get_y (const rectangle_t *rectangle)
 {
@@ -54,6 +101,16 @@ rectangle_get_y (const rectangle_t *rectangle)
   return result;
 }
 
+/**
+ * @brief Get the width of a rectangle.
+ *
+ * This function returns the width of the given rectangle. If the input rectangle
+ * pointer is NULL, the function returns 0.
+ *
+ * @param rectangle A pointer to the rectangle to retrieve the width from.
+ *
+ * @return The width of the rectangle, or 0 if the input rectangle pointer is NULL.
+ */
 uint32_t
 rectangle_get_width (const rectangle_t *rectangle)
 {
@@ -66,6 +123,16 @@ rectangle_get_width (const rectangle_t *rectangle)
   return result;
 }
 
+/**
+ * @brief Get the height of a rectangle.
+ *
+ * This function returns the height of the given rectangle. If the input rectangle
+ * pointer is NULL, the function returns 0.
+ *
+ * @param rectangle A pointer to the rectangle to retrieve the height from.
+ *
+ * @return The height of the rectangle, or 0 if the input rectangle pointer is NULL.
+ */
 uint32_t
 rectangle_get_height (const rectangle_t *rectangle)
 {
